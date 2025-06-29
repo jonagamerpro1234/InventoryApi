30133b99e4e95539062ab408e1e61ac7f8c26079
SrJss <31166687+jonagamerpro1234@users.noreply.github.com>
2025-06-29 17:14:41 -0400

    bug fix

--- src/main/java/jss/inventoryApi/InventoryListener.java
+++ src/main/java/jss/inventoryApi/InventoryListener.java
@@ -5,7 +5,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
-import org.bukkit.inventory.Inventory;
 import org.jetbrains.annotations.NotNull;
 
 public class InventoryListener implements Listener {
